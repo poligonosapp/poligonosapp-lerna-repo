a770d552328ac81007bdca07adab4d668abcf99d
Luis Mendes <machadoluismendes@gmail.com>
2021-05-13 10:47:55 -0300

    new Dockerfile emulator failing

--- packages/flutter_map/example/android/app/src/main/kotlin/me/jpryan/flutter_map_example/MainActivity.kt
+++ packages/flutter_map/example/android/app/src/main/kotlin/me/jpryan/flutter_map_example/MainActivity.kt
@@ -2,5 +2,25 @@ package me.jpryan.flutter_map_example
 
 import io.flutter.embedding.android.FlutterActivity
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
 class MainActivity: FlutterActivity() {
+
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
+                        // Your existing code
+                }
+        );
+    }
+
+
 }
